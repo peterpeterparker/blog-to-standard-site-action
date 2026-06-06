b0a7835224cafa072f403dd61f1457442e555d3d
David Dal Busco <david.dalbusco@outlook.com>
2026-06-06 16:41:08 +0200

    feat: no commit if no files to commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,11 @@ set -e
 
 /usr/src/app/action
 
+# The action pipeline does not throw an errors if there is nothing to process.
+cd $GITHUB_WORKSPACE
+
+if git diff --quiet && git diff --staged --quiet; then
+  exit 0
+fi
+
 /usr/src/scripts/create-pr
