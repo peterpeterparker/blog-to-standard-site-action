6fca0e98b48dd3e9f1d840dd8f0597181410170f
David Dal Busco <david.dalbusco@outlook.com>
2026-06-07 08:04:18 +0200

    fix: git diff warning

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,9 @@ set -e
 # The action pipeline does not throw an errors if there is nothing to process.
 cd $GITHUB_WORKSPACE
 
+# Required to run the git commands in the container
+git config --global --add safe.directory $GITHUB_WORKSPACE
+
 if git diff --quiet && git diff --staged --quiet; then
   exit 0
 fi
